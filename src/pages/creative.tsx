f401099317e6a0368751dee6c0b6d5977ac61d19
Todd Carlsson <toddguitarc@gmail.com>
2024-03-14 12:07:10 +1300

    update font in _app file so that the same font is used across all pages

--- src/pages/creative.tsx
+++ src/pages/creative.tsx
@@ -1,12 +1,8 @@
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function CreativePage() {
     return (
         <div
             style={{ padding: 100, fontSize: 40 }}
-            className={inter.className}
         >
             Creative Page
         </div>
--- src/pages/design.tsx
+++ src/pages/design.tsx
@@ -1,12 +1,8 @@
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function DesignPage() {
     return (
         <div
             style={{ padding: 100, fontSize: 40 }}
-            className={inter.className}
+            
         >
             Design Page
         </div>
