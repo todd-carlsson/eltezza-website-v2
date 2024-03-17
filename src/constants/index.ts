a604f0bdbabe4ed0bc1e13b34e7e714aa6609524
Todd Carlsson <toddguitarc@gmail.com>
2024-03-17 15:49:33 +1300

    add text from figma file to constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,213 @@
+// WEBSITE DATA
+export const navigationLinks = [
+    {
+        title: "Our work",
+        href: "#ourWork"
+    },
+    {
+        title: "What we do",
+        href: "#whatWeDo"
+    },
+    {
+        title: "Clients",
+        href: "#clients"
+    },
+    {
+        title: "about us",
+        href: "#about"
+    },
+    {
+        title: "Get in touch",
+        href: "#getInTouch"
+    },
+]
+
+export const headerDescription = {
+    design: "An innovative creative powerhouse, based in a dynamic studio. We craft stories that resonate positively in our culture spanning every social channel - from user-centric web experiences to bite-sized content and beyond.",
+    creative: "Enter Eltezza Creative, where we're not just content creators; we're storytellers crafting authentic, scroll-stopping content. Collaborating with amazing brands, our space thrives on genuine creativity and remarkable results."
+}
+
+export const services = {
+    design: [
+        {
+            id: "1",
+            title: "WEB DEVELOPMENT",
+            description: "From coding to launch, our web development team is your path to a digital masterpiece. Empower your business with a website that impresses and converts."
+        },
+        {
+            id: "2",
+            title: "WEB DESIGN",
+            description: "Our web design services blend aesthetics with user experience because we understand first impressions matter, and your website is your introduction."
+        },
+        {
+            id: "3",
+            title: "BRANDING",
+            description: "Our expert team specializes in the art of crafting distinctive and unforgettable brand identities. This includes the creation of unique logos, compelling messaging, and captivating visual elements."
+        },
+        {
+            id: "4",
+            title: "GRAPHIC DESIGN",
+            description: "Our designs at Eltezza are not just visually pleasing; they're strategically crafted to meet your objectives. We work hand in hand with you to create attention-grabbing assets that get your message across effectively."
+        },
+        {
+            id: "5",
+            title: "SOCIAL MEDIA",
+            description: "We start by creating a unique social media kit that aligns with your brand. Our team then curates high-quality, purposeful content strategies. We actively participate in the content collection to guarantee your social channels capture attention. With this, we then leverage our expertise and deliver captivating, scroll-stopping content."
+        }
+    ],
+
+    creative: [
+        {
+            id: "1",
+            title: "PROMOTIONAL VIDEOS",
+            description: "We create promotional videos that are more than just stories; they're connections with your audience. From brainstorming wild ideas to bringing them to life on screen, we turn brand tales into visual experiences. Join us, and let’s make your brand story one that resonates, connects, and converts."
+        },
+        {
+            id: "2",
+            title: "POST PRODUCTION",
+            description: "Transform your content game with our post-production finesse. We take raw footage and sculpt it into polished masterpieces, ensuring each frame aligns seamlessly with your brand's identity."
+        },
+        {
+            id: "3",
+            title: "ADVERTISING IMAGERY",
+            description: "We carefully curate striking visuals that communicate your brand's essence, creating a lasting impression across diverse channels. Releasing a new product or planning for an upcoming marketing campaign? Get in touch and let's see what we can cook up together - so many other brands already have 😉"
+        },
+        {
+            id: "4",
+            title: "PAID ADS MANAGEMENT",
+            description: "We craft ad campaigns to elevate your brand above digital noise, ensuring it resonates with your audience. From engaging ad content to meticulous tracking of performance metrics, we make sure that you are put in front of the right people. Pair that with continuous optimization, and the possibilities are endless."
+        }
+    ]
+}
+
+export const process = {
+    design: [
+        {
+            id: "1",
+            title: "DISCOVERY"
+        },
+        {
+            id: "2",
+            title: "AUDIT"
+        },
+        {
+            id: "3",
+            title: "CONCEPT & CREATION"
+        },
+        {
+            id: "4",
+            title: "REVIEW"
+        }
+    ],
+
+    creative: [
+        {
+            id: "1",
+            title: "DISCOVERY"
+        },
+        {
+            id: "2",
+            title: "RESEARCH"
+        },
+        {
+            id: "3",
+            title: "QUOTE"
+        },
+        {
+            id: "4",
+            title: "PRODUCTION"
+        },
+        {
+            id: "5",
+            title: "DELIVERY"
+        }
+    ]
+}
+
+export const teamDescription = "Get acquainted with the team pulling the strings behind the mess and outrageous rigs..."
+
+export const faqs = {
+    design: [
+        {
+            id: "1",
+            question: "How do I request a quote or estimate for a project?",
+            answer: "Quite simply, scroll to the bottom of our site and submit your request!"
+        },
+        {
+            id: "2",
+            question: "What industries does your agency specialize in?",
+            answer: "We’ve worked with many different clients, ranging from construction all the way to museums. If we are a good fit, no creative endeavor is beyond our capabilities."
+        },
+        {
+            id: "3",
+            question: "What is the typical timeline for project completion?",
+            answer: "Depending on the services you require, our timelines range from 3 weeks to 12 weeks. We work on a monthly basis with a lot of our clients, let’s hope we do the same for you!"
+        },
+        {
+            id: "4",
+            question: "How much do you charge?",
+            answer: "We work on project-cost pricing, which ensures we can amend the work to your vision without charging for extra hours (if required). Rest assured, we don’t charge for the sake of it, we carefully analyze the problem, craft the solution, and then deploy our strategies within a budget that suits both parties."
+        },
+        {
+            id: "5",
+            question: "Do you offer ongoing support or maintenance for projects after completion?",
+            answer: "We offer web maintenance for our web projects, this will allow us to assist you with any updates you may have in the future on top of keeping you safe from any potential cyber-attacks. Whatever the project, we will always be here to help."
+        },
+        {
+            id: "6",
+            question: "What sets your creative agency apart from others in the industry?",
+            answer: "Quite simply, scroll It’s quite simple, the level of care and attention we give to all of our clients is second to none. We want YOU to succeed as badly as WE want to breathe. the bottom of our site and submit your request!"
+        }
+    ],
+    
+    creative: [
+        {
+            id: "1",
+            question: "How do I request a quote or estimate for a project?",
+            answer: "Quite simply, scroll to the bottom of our site and submit your request!"
+        },
+        {
+            id: "2",
+            question: "What industries do you specialize in?",
+            answer: "Our bread & butter are FMCG brands. Spanning from drinks all the way to crackers, we have the the know-how to get you seen."
+        },
+        {
+            id: "3",
+            question: "What is the typical timeline for project completion?",
+            answer: "Depending on the services you require, our timeline ranges from 2 to 6 weeks."
+        },
+        {
+            id: "4",
+            question: "Does this stuff even work?",
+            answer: "Well, we probably wouldn’t be here if it didn’t ..."
+        },
+        {
+            id: "5",
+            question: "How much do you charge?",
+            answer: "We work on project-cost pricing, which ensures we can amend the work to your vision without charging for extra hours (if required). Rest assured, we don’t charge for the sake of it, we carefully analyze the problem, craft the solution, and then deploy our strategies within a budget that suits both parties."
+        },
+        {
+            id: "6",
+            question: "What sets your creative agency apart from others in the industry?",
+            answer: "It’s quite simple, the level of care and attention we give to all of our clients is second to none. We want YOU to succeed as badly as WE want to breathe"
+        }
+    ]
+}
+
+export const contactFormText = {
+    title: "Get in touch",
+    description: "Ready to turn your ideas into digital masterpieces? Let's chat over coffee!",
+    submitted: "Your connection means the world to us, and we can't wait to explore how we can make your experience even better. Whether you have questions, ideas, or just want to chat, we're here for you."
+}
+
+export const socialLinks = {
+    facebook: "https://www.facebook.com/eltezzaltd",
+    instagram: "https://www.instagram.com/eltezzacreative/",
+    linkedin: "https://www.linkedin.com/company/eltezza/"
+}
+
+
+// MOCK DATA
 export const landingPageMockData = [
     {
         id: "1",
