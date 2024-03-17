955802eee7cd26dc2607498ca24b5e9293a91f09
Todd Carlsson <toddguitarc@gmail.com>
2024-03-17 22:03:35 +1300

    add function to track page changes in _app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,21 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { poppins } from "@/fonts";
+import { useEffect } from "react";
+import Router from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const checkPage = () => {
+      console.log('PAGE CHANGED')
+    }
+
+    Router.events.on('routeChangeStart', checkPage); // add listener
+
+    return () => {
+      Router.events.off('routeChangeStart', checkPage); // remove listener
+    }
+  }, []);
   return <main className={poppins.variable}>
     <Component {...pageProps} />
   </main>;
