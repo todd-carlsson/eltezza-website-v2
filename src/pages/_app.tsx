0f36ea04370e5972e764f4ebff066decd9a3d4a0
Todd Carlsson <toddguitarc@gmail.com>
2024-03-20 11:29:33 +1300

    remove page tracking function in _app file as home page overflow is now fixed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,10 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { poppins, sourceSans } from "@/fonts";
-import { useEffect } from "react";
-import Router from 'next/router';
 import Layout from "./layout";
 import classNames from "classnames";
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    const checkPage = () => {
-      console.log('PAGE CHANGED')
-    }
-
-    Router.events.on('routeChangeStart', checkPage); // add listener
-
-    return () => {
-      Router.events.off('routeChangeStart', checkPage); // remove listener
-    }
-  }, []);
   return <main className={classNames(poppins.variable, sourceSans.variable)}>
       <Layout>
       <Component {...pageProps} />
