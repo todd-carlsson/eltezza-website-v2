a4dd138d447061dc1b668d9aa6859d78ca29f28c
Todd Carlsson <toddguitarc@gmail.com>
2024-03-20 12:55:00 +1300

    resolve import aliases in storybook main file - (this was a mission!)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import path from 'path';
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -14,14 +15,21 @@ const config: StorybookConfig = {
     name: "@storybook/nextjs",
     options: {},
   },
+  webpackFinal: async (config) => {
+    if(config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@/hooks": path.resolve("./src/hooks"),
+        "@/types": path.resolve("./src/types"),
+        "@/styles": path.resolve("./src/styles"),
+        "@/fonts": path.resolve("./src/fonts"),
+      }
+    }
+    return config;
+  },
   docs: {
     autodocs: "tag",
   },
-  staticDirs: [
-    {
-      from: '../src/fonts.ts',
-      to: 'src/fonts.ts',
-    },
-  ], 
+  staticDirs: ["..\\public"],
 };
 export default config;
