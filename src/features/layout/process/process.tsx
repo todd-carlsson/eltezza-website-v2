4620937785d5d5319a3c6ef7ab18ee82a2f67f11
Todd Carlsson <toddguitarc@gmail.com>
2024-03-19 15:01:18 +1300

    add stagger animation for process section

--- src/features/layout/process/process.tsx
+++ src/features/layout/process/process.tsx
@@ -1,4 +1,8 @@
+"use client"
+
+/* eslint-disable @next/next/no-img-element */
 import styles from "./process.module.scss"
+import { motion } from "framer-motion"
 
 interface ProcessProps {
     content: Array<ProcessPropObj>
@@ -11,36 +15,71 @@ type ProcessPropObj = {
 }
 
 export function Process({
-    content
+    content,
+    color
 }: ProcessProps) {
+
+    const boxVariant = {
+        hidden: {
+            x: -2
+        },
+        visible: {
+            x: 0,
+            transition: {
+                delay: 0.2,
+                when: "beforeChildren",
+                staggerChildren: 0.5,
+            },
+        }
+    }
+
+    const listVariant = {
+        hidden: {
+            opacity: 0
+        },
+        visible: {
+            opacity: 1,
+            transition: {
+                duration: 1
+            } 
+        }
+    }
     return (
         <section className={styles.processSection}>
             <h1 className="largeText">
                 OUR <span className="textGradient">PROCESS</span>
             </h1>
-            <div
+            <motion.div
                 className={styles.processContainer}
                 style={{
                     height: ((content.length - 1) * 50) + 100
                 }}
+                variants={boxVariant}
+                initial="hidden"
+                whileInView="visible"
             >
                 {content.map((item, i) => (
                     <>
-                        <div
+                        <motion.div
                             key={item.id}
                             className={styles.processCard}
                             style={{
                                 top: i * 50
                             }}
                         >
+                            <motion.div
+                                className={styles.background}
+                                style={{ backgroundColor: `var(${color})` }}
+                                variants={listVariant}
+                            />
                             <h3 className={styles.processText}>{item.title}</h3>
                             <h3 className={styles.processNumber}>{`0${item.id}`}</h3>
                             <img className={styles.arrow} src="/process_arrow.svg" alt="arrow" />
-                        </div>
-                        
+                        </motion.div>
+
                     </>
                 ))}
-            </div>
+            </motion.div>
         </section>
     )
 }
