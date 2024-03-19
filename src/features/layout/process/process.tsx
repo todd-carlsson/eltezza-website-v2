2f1f4958b584a98d4dab2c65950c94b63d8d6571
Todd Carlsson <toddguitarc@gmail.com>
2024-03-19 14:38:27 +1300

    add text + arrow to process section

--- src/features/layout/process/process.tsx
+++ src/features/layout/process/process.tsx
@@ -18,14 +18,27 @@ export function Process({
             <h1 className="largeText">
                 OUR <span className="textGradient">PROCESS</span>
             </h1>
-            <div className={styles.processContainer}>
+            <div
+                className={styles.processContainer}
+                style={{
+                    height: ((content.length - 1) * 50) + 100
+                }}
+            >
                 {content.map((item, i) => (
-                    <div
-                        key={item.id}
-                        className={styles.processCard}
-                    >
-
-                    </div>
+                    <>
+                        <div
+                            key={item.id}
+                            className={styles.processCard}
+                            style={{
+                                top: i * 50
+                            }}
+                        >
+                            <h3 className={styles.processText}>{item.title}</h3>
+                            <h3 className={styles.processNumber}>{`0${item.id}`}</h3>
+                            <img className={styles.arrow} src="/process_arrow.svg" alt="arrow" />
+                        </div>
+                        
+                    </>
                 ))}
             </div>
         </section>
