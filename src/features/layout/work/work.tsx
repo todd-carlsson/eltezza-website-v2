e05e3efa7ffd149d7885268616ea3313cd21f8f6
Todd Carlsson <toddguitarc@gmail.com>
2024-03-20 10:04:43 +1300

    add wheel + text to work design section

--- src/features/layout/work/work.tsx
+++ src/features/layout/work/work.tsx
@@ -1,6 +1,7 @@
 import { CarouselData } from "@/types"
 import styles from "./work.module.scss"
 import { AutoplayCarousel } from "@/features/ui"
+import Image from "next/image"
 
 interface WorkProps {
     content: Array<CarouselData>
@@ -11,16 +12,30 @@ export function Work({
     content,
     page
 }: WorkProps) {
-    return (
-        <section className={styles.workSection}>
-            {page === "design" ?
+    if (page === "design") {
+        return (
+            <section className={styles.workSectionDesign}>
+                <Image
+                    className={styles.wheel}
+                    src="/latest_works_wheel.svg"
+                    alt="Latest Work"
+                    width={175}
+                    height={175}
+                />
                 <AutoplayCarousel
                     imageOnly
                     orientation="horizontal"
                     content={content}
                 />
-                : "creative"
-            }
+                <p className={styles.workDesignText}>
+                    Drag images left or right to view more designs
+                </p>
+            </section>
+        )
+    }
+    else return (
+        <section className={styles.workSectionCreative}>
+            {page}
         </section>
     )
 }
