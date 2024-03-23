bb16ba76060024abee5e2a56aefcdda7e247b517
Todd Carlsson <toddguitarc@gmail.com>
2024-03-23 17:37:44 +1300

    make videos/images change automatically

--- src/features/layout/bts/bts.tsx
+++ src/features/layout/bts/bts.tsx
@@ -1,5 +1,7 @@
 import { BtsData } from "@/types";
 import styles from "./bts.module.scss";
+import { useEffect, useState } from "react";
+import classNames from "classnames";
 
 interface BTSProps {
   content: Array<BtsData>;
@@ -7,33 +9,54 @@ interface BTSProps {
 }
 
 export function BTS({ content, page }: BTSProps) {
+  const [activeImage, setActiveImage] = useState(content.length - 1);
+  const hiddenImage = activeImage === 0 ? content.length - 1 : activeImage - 1;
+
+  useEffect(() => {
+    setTimeout(() => {
+      setActiveImage(activeImage === content.length - 1 ? 0 : activeImage + 1);
+      console.log(activeImage);
+    }, 2500);
+  }, [activeImage, content]);
+
   return (
     <section className={styles.btsSection}>
       <div className={styles.btsContentContainer}>
-        {content.map((item) => {
-          if (item.isVideo) {
-            return (
-              <video
-                className={styles.btsImg}
-                key={item.id}
-                src={item.src}
-                autoPlay
-                muted
-                loop
-              />
-            );
-          } else {
-            return (
-              // eslint-disable-next-line @next/next/no-img-element
-              <img
-                className={styles.btsImg}
-                key={item.id}
-                src={item.src}
-                alt={item.alt}
-              />
-            );
-          }
-        })}
+        {/* HIDDEN */}
+        {content[hiddenImage].isVideo ? (
+          <video
+            className={classNames(styles.btsImg, styles.hiddenImage)}
+            muted
+          >
+            <source src={content[hiddenImage].src} />
+          </video>
+        ) : (
+          // eslint-disable-next-line @next/next/no-img-element
+          <img
+            className={classNames(styles.btsImg, styles.hiddenImage)}
+            src={content[hiddenImage].src}
+            alt={content[hiddenImage].alt}
+          />
+        )}
+        {/* ACTIVE */}
+        {content[activeImage].isVideo ? (
+          <video
+            className={styles.btsImg}
+            src={content[activeImage].src}
+            autoPlay
+            loop
+            muted
+          >
+            <source src={content[activeImage].src} />
+          </video>
+        ) : (
+          // eslint-disable-next-line @next/next/no-img-element
+          <img
+            className={styles.btsImg}
+            src={content[activeImage].src}
+            alt={content[activeImage].alt}
+          />
+        )}
       </div>
       <div>{page}</div>
     </section>
