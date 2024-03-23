d5393c88b253a9fc9ffc141a0ccd87fa5cfede69
Todd Carlsson <toddguitarc@gmail.com>
2024-03-23 18:32:55 +1300

    add and style text for bts section

--- src/features/layout/bts/bts.tsx
+++ src/features/layout/bts/bts.tsx
@@ -2,6 +2,7 @@ import { BtsData } from "@/types";
 import styles from "./bts.module.scss";
 import { useEffect, useState } from "react";
 import classNames from "classnames";
+import { motion } from "framer-motion";
 
 interface BTSProps {
   content: Array<BtsData>;
@@ -12,6 +13,8 @@ export function BTS({ content, page }: BTSProps) {
   const [activeImage, setActiveImage] = useState(content.length - 1);
   const hiddenImage = activeImage === 0 ? content.length - 1 : activeImage - 1;
 
+  const gradientText = page === "design" ? "CURTAINS" : "MESSY!";
+
   useEffect(() => {
     setTimeout(() => {
       setActiveImage(activeImage === content.length - 1 ? 0 : activeImage + 1);
@@ -19,20 +22,39 @@ export function BTS({ content, page }: BTSProps) {
     }, 2500);
   }, [activeImage, content]);
 
+  const animationVariant = {
+    initial: {
+      x: -1000,
+    },
+    animate: {
+      x: 0,
+      transition: {
+        ease: "easeOut",
+        duration: 0.35,
+      },
+    },
+  };
+
   return (
     <section className={styles.btsSection}>
       <div className={styles.btsContentContainer}>
         {/* HIDDEN */}
         {content[hiddenImage].isVideo ? (
-          <video
+          <motion.video
+            variants={animationVariant}
+            animate="animate"
+            initial="initial"
             className={classNames(styles.btsImg, styles.hiddenImage)}
             muted
           >
             <source src={content[hiddenImage].src} />
-          </video>
+          </motion.video>
         ) : (
           // eslint-disable-next-line @next/next/no-img-element
-          <img
+          <motion.img
+            variants={animationVariant}
+            animate="animate"
+            initial="initial"
             className={classNames(styles.btsImg, styles.hiddenImage)}
             src={content[hiddenImage].src}
             alt={content[hiddenImage].alt}
@@ -40,7 +62,10 @@ export function BTS({ content, page }: BTSProps) {
         )}
         {/* ACTIVE */}
         {content[activeImage].isVideo ? (
-          <video
+          <motion.video
+            variants={animationVariant}
+            animate="animate"
+            initial="initial"
             className={styles.btsImg}
             src={content[activeImage].src}
             autoPlay
@@ -48,17 +73,25 @@ export function BTS({ content, page }: BTSProps) {
             muted
           >
             <source src={content[activeImage].src} />
-          </video>
+          </motion.video>
         ) : (
           // eslint-disable-next-line @next/next/no-img-element
-          <img
+          <motion.img
+            variants={animationVariant}
+            animate="animate"
+            initial="initial"
             className={styles.btsImg}
             src={content[activeImage].src}
             alt={content[activeImage].alt}
           />
         )}
       </div>
-      <div>{page}</div>
+      <div className={styles.textContainer}>
+        <h1 className={classNames("largeText", styles.btsText)}>
+          {page === "design" ? "TAKE A PEAK BEHIND THE " : "THE PROCESS IS "}
+          <span className="textGradient">{gradientText}</span>
+        </h1>
+      </div>
     </section>
   );
 }
