f7eb158e8572bc9cb82c59fa22f406da062e321a
Todd Carlsson <toddguitarc@gmail.com>
2024-03-25 12:23:16 +1300

    add some styling

--- src/features/layout/contact/contact.tsx
+++ src/features/layout/contact/contact.tsx
@@ -1,5 +1,14 @@
 import styles from "./contact.module.scss";
 
 export function Contact() {
-  return <section className={styles.contactSection}>Contact</section>;
+  return (
+    <section className={styles.contactSection}>
+      <div className={styles.purpleOrb} />
+      <div className={styles.contactContainer}>
+        <h1 className="largeText">LET&apos;S CONNECT &</h1>
+        <h1 className="largeText">DESIGN TOGETHER</h1>
+      </div>
+      <div className={styles.orangeOrb} />
+    </section>
+  );
 }
