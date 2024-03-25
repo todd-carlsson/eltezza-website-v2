a90731a6ca5507fa75dd5624a9afb6b3e2cf5513
Todd Carlsson <toddguitarc@gmail.com>
2024-03-25 13:19:35 +1300

    finish styling input + document in storybook

--- src/features/ui/input/input.tsx
+++ src/features/ui/input/input.tsx
@@ -3,14 +3,20 @@ import styles from "./input.module.scss";
 import * as React from "react";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  error?: boolean;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, error, ...props }, ref) => {
     return (
       <input
         type={type}
-        className={classNames(styles.input, className)}
+        className={classNames(
+          styles.input,
+          error ? styles.error : "",
+          className,
+        )}
         ref={ref}
         {...props}
       />
