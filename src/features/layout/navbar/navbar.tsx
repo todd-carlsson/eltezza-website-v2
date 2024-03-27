4449725793e87442258da044597c9a4295297e92
Todd Carlsson <133827780+todd-carlsson@users.noreply.github.com>
2024-03-27 18:12:35 +1300

    Add active state to navbar + minor style tweaks (#29)

--- src/features/layout/navbar/navbar.tsx
+++ src/features/layout/navbar/navbar.tsx
@@ -5,21 +5,52 @@ import styles from "./navbar.module.scss";
 import { navigationLinks } from "@/constants";
 import Image from "next/image";
 import useWindowSize from "@/hooks/useWindowSize";
+import { useState } from "react";
+import classNames from "classnames";
 
 export function Navbar() {
   const [windowWidth] = useWindowSize();
+
+  const [activeLink, setActiveLink] = useState("-1");
+
+  function clickHandler(href: string) {
+    setActiveLink(href);
+  }
+
   return (
     <nav className={styles.navbar}>
       <ul className={styles.navbarContainer}>
         {windowWidth > 800
           ? navigationLinks.desktop.map((link) => (
-              <Link key={link.href} href={link.href}>
-                <li className={styles.link}>{link.title}</li>
+              <Link
+                onClick={() => clickHandler(link.href)}
+                key={link.href}
+                href={link.href}
+              >
+                <li
+                  className={classNames(
+                    styles.link,
+                    activeLink === link.href && styles.linkActive,
+                  )}
+                >
+                  {link.title}
+                </li>
               </Link>
             ))
           : navigationLinks.mobile.map((link) => (
-              <Link key={link.href} href={link.href}>
-                <li className={styles.link}>{link.title}</li>
+              <Link
+                onClick={() => clickHandler(link.href)}
+                key={link.href}
+                href={link.href}
+              >
+                <li
+                  className={classNames(
+                    styles.link,
+                    activeLink === link.href && styles.linkActive,
+                  )}
+                >
+                  {link.title}
+                </li>
               </Link>
             ))}
         <li>
@@ -28,7 +59,6 @@ export function Navbar() {
             alt="^ Back to top"
             height={windowWidth > 800 ? 28 : 18}
             width={windowWidth > 800 ? 28 : 18}
-            // className={styles.link}
           />
         </li>
       </ul>
