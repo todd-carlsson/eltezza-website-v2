36288ac65a2b8bac707977e3fea1d91a369f5f3f
Todd Carlsson <toddguitarc@gmail.com>
2024-03-31 13:57:36 +1300

    turn headless off in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,6 @@ export default defineConfig({
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "http://localhost:3000",
 
