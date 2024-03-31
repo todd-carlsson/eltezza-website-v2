f706ba0c5fbe4f1c5c8b0c9000e670eecb5ed9b8
Todd Carlsson <toddguitarc@gmail.com>
2024-03-31 14:24:00 +1300

    add webServer option in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,6 +22,11 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  webServer: {
+    command: "npm run start",
+    url: "http://localhost:3000/",
+    timeout: 120000,
+  },
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "http://localhost:3000",
