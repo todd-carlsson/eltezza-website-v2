0684a617f0e1aa6b17360a341bbcc61ac90e5c7b
Todd Carlsson <133827780+todd-carlsson@users.noreply.github.com>
2024-04-09 13:09:07 +1200

    Reviews slider (#51)
    * add responsiveness to contact section
    
    * add basic drag animation to reviews section
    
    * add autoplay to review slider
    
    * remove autoplay

--- src/features/layout/reviews/reviews.tsx
+++ src/features/layout/reviews/reviews.tsx
@@ -1,22 +1,53 @@
 import { TestimonialsData } from "@/types";
 import Review from "./review";
 import styles from "./reviews.module.scss";
+import { motion, useMotionValue } from "framer-motion";
+import { useState } from "react";
 
 interface ReviewsProps {
   content: Array<TestimonialsData>;
 }
 
+const DRAG_BUFFER = 80;
+
 export function Reviews({ content }: ReviewsProps) {
+  const [imgIndex, setImgIndex] = useState(0);
+
+  const dragX = useMotionValue(0);
+
+  function onDragEnd() {
+    const x = dragX.get();
+    if (x <= -DRAG_BUFFER && imgIndex !== content.length - 1) {
+      setImgIndex((prevState) => prevState + 1);
+    } else if (x >= DRAG_BUFFER && imgIndex !== 0) {
+      setImgIndex((prevState) => prevState - 1);
+    }
+  }
+
   return (
     <section className={styles.reviews}>
       <h1 className="largeText">
         Hear it from our <span className="textGradient">partners</span>
       </h1>
-      <div className={styles.reviewsContainer}>
-        {content.map((item) => (
-          <Review key={item.id} review={item} />
+      <motion.div
+        drag="x"
+        dragConstraints={{
+          left: 0,
+          right: 0,
+        }}
+        onDragEnd={onDragEnd}
+        style={{
+          x: dragX,
+        }}
+        animate={{
+          translateX: `-${imgIndex * 33}vw`,
+        }}
+        className={styles.reviewsContainer}
+      >
+        {content.map((item, i) => (
+          <Review key={item.id} review={item} index={i} imgIndex={imgIndex} />
         ))}
-      </div>
+      </motion.div>
     </section>
   );
 }
