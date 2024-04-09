a0f4c6eb35eadf12a07a454c5eed258128129073
Todd Carlsson <toddguitarc@gmail.com>
2024-04-09 16:08:04 +1200

    add layout prop to h1 and fix animation

--- src/features/layout/accordion/faq.tsx
+++ src/features/layout/accordion/faq.tsx
@@ -27,17 +27,18 @@ export function Faq({ content, color }: AccordionProps) {
     } else return "#fff";
   }
 
-  function getTitleAnimation(id: string) {
+  function getDescriptionPosition(id: string) {
     if (windowWidth > 800) {
-      if (active === id) return -40;
-      else return 0;
-    } else return 0;
+      if (active === id) {
+        return "relative";
+      } else return "absolute";
+    } else return "relative";
   }
 
   function getDescriptionAnimation(id: string) {
     if (windowWidth > 800) {
       if (active === id) return 0;
-      else return 20;
+      else return 65;
     } else return 0;
   }
   return (
@@ -58,12 +59,11 @@ export function Faq({ content, color }: AccordionProps) {
             }}
           >
             <motion.h1
+              layout
               initial={{
                 y: windowWidth > 800 ? -40 : 0,
               }}
-              animate={{
-                y: getTitleAnimation(item.id),
-              }}
+              transition={{ duration: 0.2 }}
               className={classNames(
                 styles.accordionTitleFaq,
                 item.wrap ? "" : styles.noWrap,
@@ -86,6 +86,7 @@ export function Faq({ content, color }: AccordionProps) {
               className={classNames(styles.descriptionFaq)}
               style={{
                 color: color === "--adobe-purple" ? "#fff" : "#000",
+                position: getDescriptionPosition(item.id),
               }}
             >
               {item.description}
