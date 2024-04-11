ee91da940b75ec8547a3da19c5bf224915d6d84a
Todd Carlsson <133827780+todd-carlsson@users.noreply.github.com>
2024-04-11 17:10:36 +1200

    Responsiveness (#58)
    * add responsiveness to contact section
    
    * add responsiveness for large screens
    
    * reorder team data
    
    * reduce width for large screens for design work section
    
    * add max width for creative work videos
    
    * add extra mobile responsiveness to hero section + largeText responsiveness
    
    * add responsiveness to reviews section
    
    * fix responsiveness to accordion
    
    * mor minor responsive fixes

--- src/features/layout/reviews/reviews.tsx
+++ src/features/layout/reviews/reviews.tsx
@@ -4,6 +4,7 @@ import styles from "./reviews.module.scss";
 import { motion, useMotionValue } from "framer-motion";
 import uuid from "react-uuid";
 import { useState } from "react";
+import useWindowSize from "@/hooks/useWindowSize";
 
 interface ReviewsProps {
   content: Array<TestimonialsData>;
@@ -13,6 +14,7 @@ const DRAG_BUFFER = 80;
 
 export function Reviews({ content }: ReviewsProps) {
   const [imgIndex, setImgIndex] = useState(0);
+  const [windowWidth] = useWindowSize();
 
   const dragX = useMotionValue(0);
   // const scale = useTransform(dragX, [-DRAG_BUFFER, 0, DRAG_BUFFER], [1, 0.8, 1])
@@ -40,35 +42,48 @@ export function Reviews({ content }: ReviewsProps) {
       return "+33.333vw";
     } else return `-${imgIndex * 33.333}vw`;
   }
-
-  return (
-    <section className={styles.reviews}>
-      <h1 className="largeText">
-        Hear it from our <span className="textGradient">partners</span>
-      </h1>
-      <motion.div
-        drag="x"
-        dragConstraints={{
-          left: 0,
-          right: 0,
-        }}
-        onDragEnd={onDragEnd}
-        style={{
-          x: dragX,
-        }}
-        animate={{
-          translateX: getAnimationValue(),
-          transition: {
-            duration: 0.35,
-            type: "tween",
-          },
-        }}
-        className={styles.reviewsContainer}
-      >
-        {content.map((item, i) => (
-          <Review key={uuid()} review={item} index={i} imgIndex={imgIndex} />
-        ))}
-      </motion.div>
-    </section>
-  );
+  if (windowWidth > 1000) {
+    return (
+      <section className={styles.reviews}>
+        <h1 className="largeText">
+          Hear it from our <span className="textGradient">partners</span>
+        </h1>
+        <motion.div
+          drag="x"
+          dragConstraints={{
+            left: 0,
+            right: 0,
+          }}
+          onDragEnd={onDragEnd}
+          style={{
+            x: dragX,
+          }}
+          animate={{
+            translateX: getAnimationValue(),
+            transition: {
+              duration: 0.35,
+              type: "tween",
+            },
+          }}
+          className={styles.reviewsContainer}
+        >
+          {content.map((item, i) => (
+            <Review key={uuid()} review={item} index={i} imgIndex={imgIndex} />
+          ))}
+        </motion.div>
+      </section>
+    );
+  } else
+    return (
+      <section className={styles.reviews}>
+        <h1 className="largeText">
+          Hear it from our <span className="textGradient">partners</span>
+        </h1>
+        <div className={styles.reviewsContainer}>
+          {content.map((item, i) => (
+            <Review key={uuid()} review={item} index={i} imgIndex={imgIndex} />
+          ))}
+        </div>
+      </section>
+    );
 }
