7cd70866f6064a17c82213e5b031c0d14b72851c
Todd Carlsson <toddguitarc@gmail.com>
2024-04-11 16:12:09 +1200

    add responsiveness for large screens

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,13 @@ import type { AppProps } from "next/app";
 import { poppins, sourceSans } from "@/fonts";
 import Layout from "./layout";
 import classNames from "classnames";
+import styles from "@/styles/Home.module.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={classNames(poppins.variable, sourceSans.variable)}>
+    <main
+      className={classNames(poppins.variable, sourceSans.variable, styles.body)}
+    >
       <Layout>
         <Component {...pageProps} />
       </Layout>
