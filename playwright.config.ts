b46b813d0e8156317ae4b945dfba51b421f0fcdd
Todd Carlsson <toddguitarc@gmail.com>
2024-04-12 15:46:39 +1200

    add favicon + metadata

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "http://localhost:3000",
-
+    headless: false,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
     screenshot: "on",
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,10 +11,33 @@ export default function Home() {
     <>
       <Head>
         <title>Eltezza | Rise above the noise</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest"></link>
         <meta
           property="og:title"
           content="Eltezza | Rise above the noise"
           key="title"
+        />
+        <meta
+          property="og:description"
+          content="We move past generic concepts and incorporate unique conceptual elements to create inimitable identities. In other words, we make your brand worth looking at."
+          key="description"
         />
       </Head>
       <main className={classNames(styles.main, inter.className)}>
