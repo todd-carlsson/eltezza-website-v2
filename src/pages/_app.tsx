460c9f130224ebf2737f9b1fdb175105c6161da9
Todd Carlsson <133827780+todd-carlsson@users.noreply.github.com>
2024-04-14 12:51:59 +1200

    Page transitions (#69)
    * add responsiveness to contact section
    
    * add right marquee data for design hero section
    
    * add basic page transitions
    
    * move navbar into layout file
    
    * add privacy screen to page transitions
    
    * add privacy screen to home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,10 @@ import type { AppProps } from "next/app";
 import { poppins, sourceSans } from "@/fonts";
 import Layout from "./layout";
 import classNames from "classnames";
-import styles from "@/styles/Home.module.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main
-      className={classNames(poppins.variable, sourceSans.variable, styles.body)}
-    >
+    <main className={classNames(poppins.variable, sourceSans.variable)}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
