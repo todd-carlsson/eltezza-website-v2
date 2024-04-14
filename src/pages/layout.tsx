5f863b02654032f1936f489ba028c75e7ea788f1
Todd Carlsson <toddguitarc@gmail.com>
2024-04-14 12:17:19 +1200

    add basic page transitions

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,9 +1,22 @@
 import { Header } from "@/features/layout";
 import { useRouter } from "next/router";
 import { ReactNode } from "react";
+import styles from "@/styles/Home.module.css";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Layout({ children }: { children: ReactNode }) {
   const router = useRouter();
+  const variants = {
+    initial: {
+      opacity: 0,
+    },
+    animate: {
+      opacity: 1,
+    },
+    exit: {
+      opacity: 0,
+    },
+  };
   return (
     <>
       {router.pathname !== "/" && (
@@ -18,7 +31,21 @@ export default function Layout({ children }: { children: ReactNode }) {
           />
         </>
       )}
-      {children}
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={router.route}
+          className={styles.main}
+          variants={variants}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          transition={{
+            duration: 0.75,
+          }}
+        >
+          {children}
+        </motion.div>
+      </AnimatePresence>
     </>
   );
 }
