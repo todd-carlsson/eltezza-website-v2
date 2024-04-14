60d4235d0ffc26113427c36fb3324d81f3b36930
Todd Carlsson <toddguitarc@gmail.com>
2024-04-14 12:46:41 +1200

    add privacy screen to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,15 +4,35 @@ import { HomeComponent } from "@/features/layout";
 import classNames from "classnames";
 import MetaData from "@/metadata";
 import { metaData } from "@/constants";
+import { motion, useIsPresent } from "framer-motion";
+import { useRouter } from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const isPresent = useIsPresent();
+  const router = useRouter();
   return (
     <>
       <MetaData data={metaData.home} />
       <main className={classNames(styles.main, inter.className)}>
         <HomeComponent />
+        <motion.div
+          initial={{ scaleX: 1 }}
+          animate={{
+            scaleX: 0,
+            transition: { duration: 0.5, ease: "circOut" },
+          }}
+          exit={{ scaleX: 1, transition: { duration: 0.5, ease: "circIn" } }}
+          style={{
+            originX: isPresent ? 0 : 1,
+            backgroundColor:
+              router.pathname === "/creative"
+                ? "var(--adobe-purple)"
+                : "var(--ez-orange)",
+          }}
+          className={styles.privacyScreen}
+        />
       </main>
     </>
   );
