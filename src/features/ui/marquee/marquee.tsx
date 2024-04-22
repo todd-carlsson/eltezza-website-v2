4a677d7a86e503bd8e1721094e00098eb95d9000
Todd Carlsson <toddguitarc@gmail.com>
2024-04-22 18:47:29 +1200

    add badgeSize prop to duplicate carousel item list in Marquee

--- src/features/ui/marquee/marquee.tsx
+++ src/features/ui/marquee/marquee.tsx
@@ -56,6 +56,7 @@ export function Marquee({
         {content.map((item) => (
           <CarouselItem
             key={item.id}
+            badgeSize={badgeSize}
             ariaHidden
             data={item}
             width={width}
