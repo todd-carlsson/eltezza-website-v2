c2a3b794f8cd564da940795459802faa9ef7de6b
Todd Carlsson <133827780+todd-carlsson@users.noreply.github.com>
2024-05-27 12:33:28 +1200

    Remove scroll on landing page for desktop view (#158)
    * add responsiveness to contact section
    
    * add right marquee data for design hero section
    
    * fix top position for mobile on home page
    
    * fix mobile view for home page
    
    * fix marquee gap on home page
    
    * fix home page from being off centered on mobile view
    
    * remove scroll on landing page for desktop view

--- src/features/layout/home/home.tsx
+++ src/features/layout/home/home.tsx
@@ -6,9 +6,11 @@ import { ButtonVariant } from "@/features/ui";
 import { landingPageData, logos, pageTags } from "@/constants";
 import classNames from "classnames";
 import uuid from "react-uuid";
+import useWindowSize from "@/hooks/useWindowSize";
 
 export function HomeComponent() {
   const [isDualPage, setIsDualPage] = useState(false);
+  const [windowWidth] = useWindowSize();
 
   useEffect(() => {
     setTimeout(() => {
@@ -17,101 +19,106 @@ export function HomeComponent() {
   }, []);
 
   return (
-    <div className={classNames(styles.homeContainer, "noScroll")}>
-      <motion.div
-        initial={{ opacity: 0, y: "-100vh", x: "-100vw" }}
-        animate={{ opacity: 0.4, y: !isDualPage ? 0 : "50vh", x: 0 }}
-        transition={{
-          type: "spring",
-          stiffness: 310,
-          damping: isDualPage ? 55 : 30,
-          duration: 4,
-          delay: 0.5,
-        }}
-        className={styles.orangeOrb}
-      />
-      <div className={styles.homeWrapper}>
-        <AnimatePresence>
-          {!isDualPage && (
-            <motion.div
-              initial={{ opacity: 0, y: "100vh" }}
-              animate={{
-                opacity: isDualPage ? 0 : 1,
-                y: 0,
-              }}
-              transition={{
-                ease: "easeOut",
-                type: "spring",
-                stiffness: 310,
-                damping: 30,
-                duration: 4,
-                delay: !isDualPage ? 0.5 : 0,
-              }}
-              exit={{ opacity: 0 }}
-              className={styles.homeTitleContainer}
-            >
-              <h1 className={styles.homeTitle}>RISE ABOVE</h1>
-              <h1 className={styles.homeTitle}>
-                THE{" "}
-                <span className={classNames(styles.homeTitle, "textGradient")}>
-                  NOISE
-                </span>
-              </h1>
-            </motion.div>
-          )}
-        </AnimatePresence>
-        <AnimatePresence>
-          {isDualPage && (
-            <motion.div
-              initial={{ display: "none", opacity: 0 }}
-              animate={{ display: "flex", opacity: 1 }}
-              transition={{
-                ease: "easeOut",
-                type: "spring",
-                stiffness: 310,
-                damping: 30,
-                duration: 4,
-                delay: 0.5,
-              }}
-              className={styles.dualPageContainer}
-            >
-              <PagePortal
-                key={uuid()}
-                title="design"
-                logo={logos.design}
-                content={landingPageData.design}
-                duration={16}
-                tags={pageTags.design}
-                buttonVariant={ButtonVariant.homeOrange}
-              />
-              <div className={styles.blurLeft} />
-              <div className={styles.divider} />
-              <div className={styles.blurRight} />
-              <PagePortal
-                key={uuid()}
-                title="creative"
-                logo={logos.creative}
-                content={landingPageData.creative}
-                duration={29}
-                tags={pageTags.creative}
-                buttonVariant={ButtonVariant.homePurple}
-              />
-            </motion.div>
-          )}
-        </AnimatePresence>
+    <>
+      <style>{`body { overflow-y: ${windowWidth <= 1200 ? "scroll" : "hidden"}; }`}</style>
+      <div className={classNames(styles.homeContainer)}>
+        <motion.div
+          initial={{ opacity: 0, y: "-100vh", x: "-100vw" }}
+          animate={{ opacity: 0.4, y: !isDualPage ? 0 : "50vh", x: 0 }}
+          transition={{
+            type: "spring",
+            stiffness: 310,
+            damping: isDualPage ? 55 : 30,
+            duration: 4,
+            delay: 0.5,
+          }}
+          className={styles.orangeOrb}
+        />
+        <div className={styles.homeWrapper}>
+          <AnimatePresence>
+            {!isDualPage && (
+              <motion.div
+                initial={{ opacity: 0, y: "100vh" }}
+                animate={{
+                  opacity: isDualPage ? 0 : 1,
+                  y: 0,
+                }}
+                transition={{
+                  ease: "easeOut",
+                  type: "spring",
+                  stiffness: 310,
+                  damping: 30,
+                  duration: 4,
+                  delay: !isDualPage ? 0.5 : 0,
+                }}
+                exit={{ opacity: 0 }}
+                className={styles.homeTitleContainer}
+              >
+                <h1 className={styles.homeTitle}>RISE ABOVE</h1>
+                <h1 className={styles.homeTitle}>
+                  THE{" "}
+                  <span
+                    className={classNames(styles.homeTitle, "textGradient")}
+                  >
+                    NOISE
+                  </span>
+                </h1>
+              </motion.div>
+            )}
+          </AnimatePresence>
+          <AnimatePresence>
+            {isDualPage && (
+              <motion.div
+                initial={{ display: "none", opacity: 0 }}
+                animate={{ display: "flex", opacity: 1 }}
+                transition={{
+                  ease: "easeOut",
+                  type: "spring",
+                  stiffness: 310,
+                  damping: 30,
+                  duration: 4,
+                  delay: 0.5,
+                }}
+                className={styles.dualPageContainer}
+              >
+                <PagePortal
+                  key={uuid()}
+                  title="design"
+                  logo={logos.design}
+                  content={landingPageData.design}
+                  duration={16}
+                  tags={pageTags.design}
+                  buttonVariant={ButtonVariant.homeOrange}
+                />
+                <div className={styles.blurLeft} />
+                <div className={styles.divider} />
+                <div className={styles.blurRight} />
+                <PagePortal
+                  key={uuid()}
+                  title="creative"
+                  logo={logos.creative}
+                  content={landingPageData.creative}
+                  duration={29}
+                  tags={pageTags.creative}
+                  buttonVariant={ButtonVariant.homePurple}
+                />
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
+        <motion.div
+          initial={{ opacity: 0, y: "-100vh", x: "100vw" }}
+          animate={{ opacity: 0.4, y: !isDualPage ? 0 : "-50vh", x: 0 }}
+          transition={{
+            type: "spring",
+            stiffness: 310,
+            damping: isDualPage ? 55 : 30,
+            duration: 4,
+            delay: 0.5,
+          }}
+          className={styles.purpleOrb}
+        />
       </div>
-      <motion.div
-        initial={{ opacity: 0, y: "-100vh", x: "100vw" }}
-        animate={{ opacity: 0.4, y: !isDualPage ? 0 : "-50vh", x: 0 }}
-        transition={{
-          type: "spring",
-          stiffness: 310,
-          damping: isDualPage ? 55 : 30,
-          duration: 4,
-          delay: 0.5,
-        }}
-        className={styles.purpleOrb}
-      />
-    </div>
+    </>
   );
 }
