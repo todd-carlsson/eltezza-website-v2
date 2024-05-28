b14421b73cb43b8ea46b817bb296f642a46f2587
Todd Carlsson <133827780+todd-carlsson@users.noreply.github.com>
2024-05-28 12:46:00 +1200

    Make Privacy Policy + Ts&Cs pages scroll to the top when visiting (#168)
    * add responsiveness to contact section
    
    * add right marquee data for design hero section
    
    * fix top position for mobile on home page
    
    * fix mobile view for home page
    
    * fix marquee gap on home page
    
    * fix home page from being off centered on mobile view
    
    * scroll privacy + ts&cs page to the top when visiting

--- src/pages/privacy-policy/index.tsx
+++ src/pages/privacy-policy/index.tsx
@@ -1,9 +1,13 @@
 import { metaData } from "@/constants";
 import { Privacy } from "@/features/layout";
 import MetaData from "@/metadata";
-import React from "react";
+import { Scroll } from "@/utils/scroll";
+import React, { useEffect } from "react";
 
 function PrivacyPolicy() {
+  useEffect(() => {
+    Scroll(0, "auto");
+  }, []);
   return (
     <>
       <MetaData data={metaData.privacy} />
--- src/pages/terms-and-conditions/index.tsx
+++ src/pages/terms-and-conditions/index.tsx
@@ -1,9 +1,13 @@
 import { metaData } from "@/constants";
 import { TermsConditions } from "@/features/layout";
 import MetaData from "@/metadata";
-import React from "react";
+import { Scroll } from "@/utils/scroll";
+import React, { useEffect } from "react";
 
 function TermsConditionsPage() {
+  useEffect(() => {
+    Scroll(0, "auto");
+  }, []);
   return (
     <>
       <MetaData data={metaData.termsConditions} />
