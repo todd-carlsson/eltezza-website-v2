d2688b471d93f7fbc0c048a017b7da315b7699ba
Todd Carlsson <toddguitarc@gmail.com>
2025-02-07 16:18:10 +1300

    add header to funnel page

--- src/pages/landing-funnel/index.tsx
+++ src/pages/landing-funnel/index.tsx
@@ -1,8 +1,22 @@
 import { FunnelHero } from "@/features/layout/hero/funnelHero";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "@/styles/Home.module.css";
 
 function LandingFunnel() {
   return (
     <>
+      <div className={styles.logoContainer}>
+        <Link href="/">
+          <Image
+            src="/images/eltezza_gradientLogo.svg"
+            alt="Eltezza"
+            width={164}
+            height={56}
+            className={styles.logo}
+          />
+        </Link>
+      </div>
       <FunnelHero />
     </>
   );
