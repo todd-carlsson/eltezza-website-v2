7253b12eaad9ea762126ade397d23a5bde1b139a
Todd Carlsson <toddguitarc@gmail.com>
2025-02-08 10:36:35 +1300

    organize funnel page into seperate features folder called project

--- src/pages/landing-funnel/index.tsx
+++ src/pages/landing-funnel/index.tsx
@@ -1,4 +1,4 @@
-import { FunnelHero } from "@/features/layout/hero/funnelHero";
+import { FunnelHero } from "@/features/project/hero/funnelHero";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "@/styles/Home.module.css";
