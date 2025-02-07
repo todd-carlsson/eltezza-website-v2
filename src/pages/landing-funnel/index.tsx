7423cbb995268a61439879fa68645d1bf2cc818c
Todd Carlsson <133827780+todd-carlsson@users.noreply.github.com>
2025-02-08 11:06:23 +1300

    Landing funnel folder cleanup (#203)
    * create landing-funnel page
    
    * add header to funnel page
    
    * add content padding to index
    
    * add metadata to landing funnel
    
    * minor fixes to funnel page
    
    * organize funnel page into seperate features folder called project
    
    * migrate layout folders into new subfolder called main and create subfolder ad-funnel in layout folder

--- src/pages/landing-funnel/index.tsx
+++ src/pages/landing-funnel/index.tsx
@@ -1,4 +1,4 @@
-import { FunnelHero } from "@/features/layout/hero/funnelHero";
+import { FunnelHero } from "@/features/layout";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "@/styles/Home.module.css";
