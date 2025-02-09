cb5efc039e4ec1f5553380d90d43acedaee629da
Todd Carlsson <toddguitarc@gmail.com>
2025-02-10 11:33:10 +1300

    start testimonials section

---
+++ src/features/layout/ad-funnel/testimonials/funnelTestimonials.tsx
@@ -0,0 +1,11 @@
+import styles from "./testimonials.module.scss";
+
+export function FunnelTestimonials() {
+  return (
+    <div className={styles.testimonialsSection}>
+      <div>
+        <h2>We partner with incredible businesses doing big things.</h2>
+      </div>
+    </div>
+  );
+}
