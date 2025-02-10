1adaf16ab31174f3cd2ec25001f30d50f6827d8a
Todd Carlsson <toddguitarc@gmail.com>
2025-02-10 14:28:22 +1300

    add max width for brands banner

--- src/features/layout/ad-funnel/brands/funnelBrands.tsx
+++ src/features/layout/ad-funnel/brands/funnelBrands.tsx
@@ -5,15 +5,17 @@ export function FunnelBrands() {
   return (
     <section className={styles.brandsSection}>
       <div className={styles.brandsBanner}>
-        <h4 className={styles.brandsBannerText}>
-          We help brands scale, increasing <br /> revenue by as much as 5x
-        </h4>
-        <Image
-          src="/images/eltezza-logo-rotated.svg"
-          width={117}
-          height={211}
-          alt="Eltezza"
-        />
+        <div className={styles.brandsBannerContainer}>
+          <h4 className={styles.brandsBannerText}>
+            We help brands scale, increasing <br /> revenue by as much as 5x
+          </h4>
+          <Image
+            src="/images/eltezza-logo-rotated.svg"
+            width={117}
+            height={211}
+            alt="Eltezza"
+          />
+        </div>
       </div>
     </section>
   );
