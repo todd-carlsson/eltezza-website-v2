811fe80fb92b277827183a0bcf704db040d138b1
Todd Carlsson <toddguitarc@gmail.com>
2025-02-11 12:49:37 +1300

    start services intro

--- src/features/layout/ad-funnel/services/funnelServices.tsx
+++ src/features/layout/ad-funnel/services/funnelServices.tsx
@@ -1,5 +1,13 @@
+import { Button, ButtonVariant } from "@/features/ui";
 import styles from "./services.module.scss";
 
 export function FunnelServices() {
-  return <section className={styles.servicesContainer}>FunnelServices</section>;
+  return (
+    <section className={styles.servicesContainer}>
+      <div className={styles.servicesIntro}>
+        <h2 className={styles.servicesIntroHeading}>Our bread & butter 🧈</h2>
+        <Button variant={ButtonVariant.applyPurple}>Apply Now</Button>
+      </div>
+    </section>
+  );
 }
