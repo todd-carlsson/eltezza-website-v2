6b06e4982ff8edbd13025ecc1989583cae77e520
Todd Carlsson <toddguitarc@gmail.com>
2025-02-11 14:34:30 +1300

    add basic styles for partnership section

--- src/features/layout/ad-funnel/partnership/funnelPartnership.tsx
+++ src/features/layout/ad-funnel/partnership/funnelPartnership.tsx
@@ -2,6 +2,13 @@ import styles from "./partnership.module.scss";
 
 export function FunnelPartnership() {
   return (
-    <section className={styles.partnershipSection}>FunnelPartnership</section>
+    <section className={styles.partnershipSection}>
+      <div className={styles.partnershipContainer}>
+        <div className={styles.firstBox}>
+          <h3>WE SHOULD WORK TOGETHER IF...</h3>
+        </div>
+        <div className={styles.secondBox}></div>
+      </div>
+    </section>
   );
 }
