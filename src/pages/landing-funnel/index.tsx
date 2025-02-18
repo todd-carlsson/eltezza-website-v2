677b5a34dfa172538da34591b4def6b4dbcf9966
Todd Carlsson <toddguitarc@gmail.com>
2025-02-18 15:12:42 +1300

    comment out video gallery

--- src/pages/landing-funnel/index.tsx
+++ src/pages/landing-funnel/index.tsx
@@ -2,7 +2,7 @@ import {
   FunnelContact,
   FunnelHeader,
   FunnelHero,
-  FunnelVideoGallery,
+  // FunnelVideoGallery,
   Reviews,
 } from "@/features/layout";
 import styles from "@/styles/Home.module.css";
@@ -12,7 +12,7 @@ import {
   funnelPartnership,
   funnelServices,
   funnelTestimonials,
-  funnelWorkVideos,
+  // funnelWorkVideos,
   metaData,
   testimonialsData,
 } from "@/constants";
@@ -59,7 +59,7 @@ function LandingFunnel() {
             firstBox={funnelPartnership.firstBox}
             secondBox={funnelPartnership.secondBox}
           />
-          <FunnelVideoGallery content={funnelWorkVideos} />
+          {/* <FunnelVideoGallery content={funnelWorkVideos} /> */}
         </div>
         <Reviews
           content={testimonialsData}
