1d63ad8c7811b1beb44fe815a8ffa2554076de07
Todd Carlsson <toddguitarc@gmail.com>
2025-04-22 17:05:50 +1200

    remove link to home page in showcase page

--- src/features/layout/ad-funnel/header/funnelHeader.tsx
+++ src/features/layout/ad-funnel/header/funnelHeader.tsx
@@ -1,19 +1,16 @@
 import Image from "next/image";
-import Link from "next/link";
 import styles from "./header.module.scss";
 
 export function FunnelHeader() {
   return (
     <header className={styles.logoContainer}>
-      <Link href="/">
-        <Image
-          src="/images/eltezza_gradientLogo.svg"
-          alt="Eltezza"
-          width={164}
-          height={56}
-          className={styles.logo}
-        />
-      </Link>
+      <Image
+        src="/images/eltezza_gradientLogo.svg"
+        alt="Eltezza"
+        width={164}
+        height={56}
+        className={styles.logo}
+      />
     </header>
   );
 }
