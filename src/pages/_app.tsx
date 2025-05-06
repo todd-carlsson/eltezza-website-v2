af328e5cdaf322f5f6a534782009435f5587f947
Todd Carlsson <133827780+todd-carlsson@users.noreply.github.com>
2025-05-06 13:25:58 +1200

    Add Pixel to _app (#257)
    * add pixel to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,14 @@ import type { AppProps } from "next/app";
 import { poppins, sourceSans } from "@/fonts";
 import Layout from "./layout";
 import classNames from "classnames";
-import { Hotjar } from "@/features/connection";
+import { Hotjar, Pixel } from "@/features/connection";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={classNames(poppins.variable, sourceSans.variable)}>
       <Layout>
         <Hotjar />
+        <Pixel />
         <Component {...pageProps} />
       </Layout>
     </main>
