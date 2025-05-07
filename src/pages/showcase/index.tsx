b41d4e9f0fbe7166cfe95085f5dce50b4f20fd28
Todd Carlsson <toddguitarc@gmail.com>
2025-05-07 12:16:05 +1200

    minor changes to pixel

--- src/pages/showcase/index.tsx
+++ src/pages/showcase/index.tsx
@@ -43,7 +43,7 @@ function LandingFunnel() {
 
   function onOpen() {
     setShowModal(true);
-    fbq.event("Schedule");
+    fbq.event("Purchase", { currency: "USD", value: 10 });
   }
   function onClose() {
     setShowModal(false);
