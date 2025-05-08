0a34a042e89dd24fafe4bb3181203393cf87fdd1
Todd Carlsson <toddguitarc@gmail.com>
2025-05-08 12:06:47 +1200

    add button icon

---
+++ src/constants/calendlyButtonText.tsx
@@ -0,0 +1,8 @@
+import { FiPhoneForwarded } from "react-icons/fi";
+
+export const calendlyButtonText = (
+  <>
+    Book a free call{" "}
+    <FiPhoneForwarded size={20} style={{ marginLeft: "0.5rem" }} />
+  </>
+);
