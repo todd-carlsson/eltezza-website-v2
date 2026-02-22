f05675e150dd2d23ade934654239e59ad7e7a593
Todd Carlsson <133827780+todd-carlsson@users.noreply.github.com>
2026-02-23 10:09:22 +1300

    Edit Facebook Pixel tracking script
    Removed Facebook Pixel script initialization and tracking.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -48,22 +48,14 @@ export default function App({ Component, pageProps }: AppProps) {
         }}
       />
         <Script
-          id="fb-pixel-pageview"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', ${fbq.FB_PIXEL_ID});
+        id="fb-pixel-pageview"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            fbq('track', 'PageView');
           `,
-          }}
-        />
+        }}
+      />
         <Component {...pageProps} />
       </Layout>
     </main>
