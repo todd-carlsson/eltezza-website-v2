6e3a73936a8e5208135172db698ba5fc28c76d02
Todd Carlsson <toddguitarc@gmail.com>
2026-03-02 11:39:06 +1300

    move pixel script to _document.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import { poppins, sourceSans } from "@/fonts";
 import Layout from "./layout";
 import classNames from "classnames";
 import { Hotjar } from "@/features/connection";
-import Script from "next/script";
 import * as fbq from "@/lib/fpixel";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
@@ -29,24 +28,6 @@ export default function App({ Component, pageProps }: AppProps) {
     <main className={classNames(poppins.variable, sourceSans.variable)}>
       <Layout>
         <Hotjar />
-        {/* Global Site Code Pixel - Facebook Pixel */}
-        <Script
-        id="fb-pixel"
-        strategy="beforeInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${fbq.FB_PIXEL_ID}');
-          `,
-        }}
-      />
         <Component {...pageProps} />
       </Layout>
     </main>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+import * as fbq from "@/lib/fpixel";
 
 export default function Document() {
   return (
@@ -7,6 +9,24 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        {/* Global Site Code Pixel - Facebook Pixel */}
+        <Script
+          id="fb-pixel"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${fbq.FB_PIXEL_ID}');
+          `,
+          }}
+        />
       </body>
     </Html>
   );
